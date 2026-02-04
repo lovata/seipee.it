c17038f444a510a74a21fce281b01675ef6e2e7f
Andrey Kharanenka <kharanenka@gmail.com>
2026-02-04 19:45:14 +0100

    #SPE-103 Move API creds to config file

---
+++ app/config/api.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'base_url' => env('APISYNC_BASE_URL'),
+    'username' => env('APISYNC_USERNAME'),
+    'password' => env('APISYNC_PASSWORD'),
+    'verify_ssl' => env('APISYNC_VERIFY_SSL'),
+    'timeout' => env('APISYNC_TIMEOUT'),
+];
